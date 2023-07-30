c938993f8cb70b4fb6bd61f3e9dceef784002cb9
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-07-30 17:14:34 +0200

    esercizi while part 1

---
+++ esercizi ciclo while python.py
@@ -0,0 +1,45 @@
+# Tracce
+# 🍰 Esercizio 1
+# Stampare i numeri interi da 1 a 10 usando un loop while.
+
+# print("esercizio 1")
+# i1 = 0
+
+# while i1 < 10:
+#     i1 += 1
+#     print(i1)
+
+# print("-------------------------------------------------------------------------------")
+
+
+# 🍰 Esercizio 2
+# Calcolare la somma dei primi n numeri interi positivi usando un loop while. L'utente deve inserire il valore di n.
+
+# nArray = [float(input()) , float(input()) , float(input())]
+# i2 = 0
+
+# while i2 < 3
+
+
+# 🍰 Esercizio 3
+# Stampare i numeri pari da 2 a 10 usando un loop while.
+
+# i3 = 0
+
+# while i3 < 9:
+#     i3 += 2
+#     print(i3)
+
+
+# 🍰 Esercizio 4
+# Chiedere all'utente di indovinare un numero intero casuale compreso tra 1 e 10. Continuare a chiedere all'utente di indovinare finché non indovina il numero corretto. Usare un loop while.
+# 🍰 Esercizio 5
+# Chiedere all'utente di inserire una stringa. Stampare la stringa al contrario usando un loop while.
+# 🍰 Esercizio 6
+# Stampare i numeri da 10 a 1 usando un loop while.
+# 🍰 Esercizio 7
+# Calcolare il fattoriale di un numero intero positivo n usando un loop while.
+# 🍰 Esercizio 8
+# Chiedere all'utente di inserire una lista di numeri interi. Stampare la somma di tutti i numeri usando un loop while.
+# 🍰 Esercizio 9
+# Chiedere all'utente di inserire una stringa. Stampare solo le consonanti della stringa usando un loop while.
