b839640dcadbd35f0ed95e8b523e55719f88984e
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-08-05 19:29:04 +0200

    altri fatti

--- esercizi ciclo while python.py
+++ esercizi ciclo while python.py
@@ -33,13 +33,80 @@
 
 # 🍰 Esercizio 4
 # Chiedere all'utente di indovinare un numero intero casuale compreso tra 1 e 10. Continuare a chiedere all'utente di indovinare finché non indovina il numero corretto. Usare un loop while.
+# i4 = 0
+# inputNum1 = float(input())
+
+# while i4 > 0:
+#     if(inputNum1 == 5):
+#         print("esatto")
+#         i +=1
+#     else:
+#         print("wrong")
+#         inputNum1 = float(input())
+
+# rand = randrange(10)
+# print(rand)
+
 # 🍰 Esercizio 5
 # Chiedere all'utente di inserire una stringa. Stampare la stringa al contrario usando un loop while.
+# str1Ex5 = input()
+# i5 = len(str1Ex5) - 1
+# s1 = ""
+
+# while i5 >= 0:
+#     s1 = s1 + str1Ex5[i5]
+#     i5 -= 1
+
+# print(s1)
+
 # 🍰 Esercizio 6
 # Stampare i numeri da 10 a 1 usando un loop while.
+# i6 = 10
+
+# while i6 > 0:
+#     print(i6)
+#     i6 -= 1 
+
 # 🍰 Esercizio 7
 # Calcolare il fattoriale di un numero intero positivo n usando un loop while.
+
+# res1Ex7 = 1
+# i7 = 2
+# floorToReach = int(input())
+
+
+# if 3 < floorToReach < 11:
+#     while i7 <= floorToReach:
+#         res1Ex7 *= i7
+#         i7 += 1   
+# print(res1Ex7)
+
+
 # 🍰 Esercizio 8
 # Chiedere all'utente di inserire una lista di numeri interi. Stampare la somma di tutti i numeri usando un loop while.
+
+# listEx8 = [int(input()) , int(input()) , int(input()) , int(input()) , int(input())]
+# i8 = len(listEx8) -1
+# resultEx8 = 0 
+
+# while i8 >= 0:
+#     resultEx8 =  resultEx8 + listEx8[i8]
+#     i8 -= 1
+
+# print(resultEx8)
+
 # 🍰 Esercizio 9
 # Chiedere all'utente di inserire una stringa. Stampare solo le consonanti della stringa usando un loop while.
+
+i9 = 0
+str1Ex9 = input()
+splitted = str1Ex9.split()
+floorToReach = len(splitted) - 1
+
+while i9 < floorToReach:
+    if(splitted[i9] == "a", splitted[i9] == "e", splitted[i9] == "i", splitted[i9] == "o", splitted[i9] == "u"):
+        i9 += 1
+    else:
+        print(splitted[i9])
+        i9 += 1
+
