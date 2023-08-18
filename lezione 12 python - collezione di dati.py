e48551dfda2e4b61ad02bfa8168dbf4c7f3d928d
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-08-18 19:13:38 +0200

    inzio collezione dati

---
+++ lezione 12 python - collezione di dati.py
@@ -0,0 +1,23 @@
+# 12 introduzione teoria a collezione di dati
+# 1 - list, tuple,set , dictionari
+# 2 - spiegazione di collezione
+
+
+# 3 - spiegazione termini
+    #  1 - ordinato: la collezione ha ordine ben definito e l0aggiunta di elementi non incide
+    #  2 - indicizzato: possiamo accedere agli elementi , cambiare indice (index)
+    #  3 - modificabile: possiamo aggiungere , cambiare e rimuovere gli elementi
+    #  4 - immutabili :non possiamo aggiungere , cambiare o rimuovere elementi
+    #  5 - permettere duplicati: possono esserci più elementi con lo stesso valore
+
+
+# 4 - le liste non sono collezioni ordinate e modificabili. permettono duplicati
+# 5 - le tuple sono collezioni ordinate ma immutabili . permettono duplicati
+# ------------------------------------------
+# 6 - i set sono collezioni non ordinate e modificabili. non permettono duplicati
+# 7 - i dictionary sono collezioni ordinate e modificabili(dalla versione 3.7). non permettono duplicati
+
+# esempio liste ---------------------------------------
+
+città = ["udine" , "milano" , "roma" , "napoli" , "venezia" , "roma"]
+valori = [10 , 65 , 23 , 54 , 21]
