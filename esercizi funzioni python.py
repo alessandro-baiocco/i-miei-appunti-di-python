4861b163f6126769adef7288efbb7f5631cc27b5
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-10-21 18:18:50 +0200

    esercizi function 1 e 2 fatti

---
+++ esercizi funzioni python.py
@@ -0,0 +1,49 @@
+
+# 🍰 Esercizio 1
+# Scrivi una funzione che prende una lista di numeri e restituisce la somma di tutti gli elementi.
+
+valori = [10 , 65 , 23 , 54 , 21]
+
+def esercizio_1(arr):
+    totale = 0
+    for n in arr:
+        totale += n
+    return totale
+
+# print(esercizio_1(valori))
+
+
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 2
+# Scrivi una funzione che prende una stringa e restituisce la stringa invertita.
+
+def esercizio_2(str):
+    return str[::-1]
+
+# print(esercizio_2("hello world"))
+
+
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 3
+# Scrivi una funzione che prende una lista di parole e restituisce una lista contenente solo le parole che iniziano con una lettera specificata.
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 4
+# Scrivi una funzione che prende una lista di numeri e restituisce una lista contenente solo i numeri pari.
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 5
+# Scrivi una funzione che prende una lista di parole e restituisce una lista contenente la lunghezza di ciascuna parola.
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 6
+# Scrivi una funzione che prende una lista di numeri e restituisce il valore massimo.
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 7
+# Scrivi una funzione che prende una lista di parole e restituisce la parola più lunga.
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 8
+# Scrivi una funzione che prende una lista di numeri e restituisce la media dei numeri.
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 9
+# Scrivi una funzione che prende una lista di parole e restituisce una lista contenente solo le parole palindrome.
+print("-------------------------------------------------------------------------------")
+# 🍰 Esercizio 10
+# Scrivi una funzione che prende una lista di numeri e restituisce una lista contenente solo i numeri maggiori di un valore specificato.
