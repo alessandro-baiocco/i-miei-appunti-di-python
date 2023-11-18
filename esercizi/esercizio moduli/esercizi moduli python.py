0f74f4bc8e9276d98699f7320f627514c15631f1
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-11-18 17:47:26 +0100

    esercizi modulo fatti

--- esercizi/esercizio moduli/esercizi moduli python.py
+++ esercizi/esercizio moduli/esercizi moduli python.py
@@ -74,12 +74,12 @@ print("-------------------------------------------------------------------------
 # Importa il modulo csv e apri il file dati.csv 
 # che contiene una tabella di dati separati da virgola. Poi leggi il contenuto del file e stampalo.
 
-# import csv 
+import csv 
 
-# with open('dati.csv', 'r') as file:
-#     lettore = csv.reader(file)
-#     for riga in lettore:
-#         print(riga)
+with open('dati.csv', 'r') as file:
+    lettore = csv.reader(file)
+    for riga in lettore:
+        print(riga)
 
 
 
