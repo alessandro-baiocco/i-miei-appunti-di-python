20a0930d098f033b1d5d49a35c05243cc97ee707
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-12-28 18:44:38 +0100

    esercizio 3 e 4 ok

--- esercizi/esercizi data python.py
+++ esercizi/esercizi data python.py
@@ -14,8 +14,9 @@ print(ora)
 print("------------------------------------------------------------------------------------------------------------")
 # 🍰 Esercizio 2
 # Scrivi un programma che prende in input una data in formato "gg/mm/aaaa" e stampa il nome del mese corrispondente.
+
 data1 = input("Inserisci una data (gg/mm/aaaa): ")
-data1 = datetime.datetime.strptime(data, "%d/%m/%Y")
+data1 = datetime.datetime.strptime(data1, "%d/%m/%Y")
 
 nomeMese = data1.strftime("%B")
 print("Il nome del mese corrispondente è:", nomeMese)
@@ -23,9 +24,26 @@ print("Il nome del mese corrispondente è:", nomeMese)
 print("------------------------------------------------------------------------------------------------------------")
 # 🍰 Esercizio 3
 # Scrivi un programma che prende in input due date e calcola la differenza in giorni tra le due.
+
+data2 = input("Inserisci una data (gg/mm/aaaa): ")
+data2 = datetime.datetime.strptime(data2, "%d/%m/%Y")
+data3 = input("Inserisci una data (gg/mm/aaaa): ")
+data3 = datetime.datetime.strptime(data3, "%d/%m/%Y")
+
+differenza = data2 - data3
+print("la differenza in giorni e di : ", differenza)
+
+
 print("------------------------------------------------------------------------------------------------------------")
 # 🍰 Esercizio 4
 # Scrivi un programma che prende in input una data e un numero di giorni, e calcola la data che viene dopo quella data del numero di giorni specificato.
+
+data4 = input("Inserisci una data (gg/mm/aaaa): ")
+numeroDiGiorni = int(input("Inserisci un numero di giorni "))
+data4 = datetime.datetime.strptime(data4, "%d/%m/%Y")
+print(data4 + datetime.timedelta(days= numeroDiGiorni))
+
+
 print("------------------------------------------------------------------------------------------------------------")
 # 🍰 Esercizio 5
 # Scrivi un programma che stampa tutti i giorni di un determinato mese e anno.
